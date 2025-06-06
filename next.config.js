bfd569e97dcb6ec0db9df8d629e17009f63f24f6
Eyseaa <85964979+Eyseaa@users.noreply.github.com>
2025-06-06 18:45:11 +0200

    Add next.config.js 1749228310819

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*' // Adjust this URL based on your PHP server
+      }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  }
+}
+
+module.exports = nextConfig
